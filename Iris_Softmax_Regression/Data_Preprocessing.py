10ff948cadeae313415e08dc3e07ecf6c3cdbaec
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-11-16 15:22:44 +0900

    Create Data_Preprocessing.py

---
+++ Iris_Softmax_Regression/Data_Preprocessing.py
@@ -0,0 +1,14 @@
+iris = load_iris()
+X = iris['data']
+y = iris['target']
+names = iris['target_names']
+feature_names = iris['feature_names']
+
+# Scale data to have mean 0 and variance 1 
+# which is importance for convergence of the neural network
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Split the data set into training and testing
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=2)
