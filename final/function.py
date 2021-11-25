6b704a6afa9e05bb12c12da3df08c56a810aa58e
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-11-25 22:42:16 +0900

    Update function.py

--- final/function.py
+++ final/function.py
@@ -62,3 +62,95 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
+   
+
+from typing import Any, Callable, cast, Dict, List, Optional, Tuple
+from PIL import Image
+
+IMG_EXTENSIONS = (".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm", ".tif", ".tiff", ".webp")
+
+from PIL import Image
+def pil_loader(path: str) -> Image.Image:
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, "rb") as f:
+        img = Image.open(f)
+        return img.convert("RGB")
+
+def default_loader(path: str) -> Any:
+    from torchvision import get_image_backend
+
+    if get_image_backend() == "accimage":
+        return accimage_loader(path)
+    else:
+        return pil_loader(path)
+
+class FoodImageFolder(torchvision.datasets.DatasetFolder):
+    """A generic data loader where the images are arranged in this way by default: ::
+        root/dog/xxx.png
+        root/dog/xxy.png
+        root/dog/[...]/xxz.png
+        root/cat/123.png
+        root/cat/nsdf3.png
+        root/cat/[...]/asd932_.png
+    This class inherits from :class:`~torchvision.datasets.DatasetFolder` so
+    the same methods can be overridden to customize the dataset.
+    Args:
+        root (string): Root directory path.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        loader (callable, optional): A function to load an image given its path.
+        is_valid_file (callable, optional): A function that takes path of an Image file
+            and check if the file is a valid file (used to check of corrupt files)
+     Attributes:
+        classes (list): List of the class names sorted alphabetically.
+        class_to_idx (dict): Dict with items (class_name, class_index).
+        imgs (list): List of (image path, class_index) tuples
+    """
+
+    def __init__(
+        self,
+        root: str,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+        loader: Callable[[str], Any] = default_loader,
+        is_valid_file: Optional[Callable[[str], bool]] = None,
+        txt_file=None
+    ):
+        super().__init__(
+            root,
+            loader,
+            IMG_EXTENSIONS if is_valid_file is None else None,
+            transform=transform,
+            target_transform=target_transform,
+            is_valid_file=is_valid_file,
+        )
+
+        if txt_file is not None:
+            f = open(txt_file, 'r')
+            id = 0
+
+            path_list = []
+            while True:
+                line = f.readline()[:-1]
+                if not line: break
+                path = root+line+".jpg"
+                path_list.append(path)
+
+            f.close()
+
+            self.imgs = []
+            for sample in self.samples:
+                if sample[0] in path_list:
+                    self.imgs.append(sample)
+            
+            self.samples = self.imgs
+        else:
+            self.imgs = self.samples
+
+        print(len(self.imgs))
+        
+        
+        
+
